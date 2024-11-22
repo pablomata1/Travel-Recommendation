e77cdda7e1f9dd660e336bb1118c4bef9233f088
pablomata1 <pablomata977@gmail.com>
2024-11-21 20:55:08 -0500

    added new functionality and fixed bugs

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,6 +1,8 @@
 // const destination = document.getElementById('destination');
 const searchBtn = document.getElementById('searchBtn');
 const resetBtn = document.getElementById('resetBtn');
+const resultsElement = document.getElementById('results');
+
 
 const keyWords = [
     {country : ['country', 'countries']},
@@ -19,7 +21,6 @@ function searchVariations(word){
         let variations = Object.values(key);
         let variationsArray = variations[0]; //Storing array of values from the variations array above.
         
-        //using find() method to search if the users input is found in the array
         //using includes() method check if the value for word is found in the array
         found = variationsArray.includes(word);
         console.log(found);
@@ -30,7 +31,7 @@ function searchVariations(word){
     }
 
     if(!found){
-        return word;
+        return word; //if not found returns user input orginal word
     }
 }
 
@@ -51,108 +52,113 @@ function getVariationWord(word){
     }
 }
 
-function searchDestination(){
-    const input = document.getElementById('destination').value.toLowerCase();
-    const resultsElement = document.getElementById('results');
-    resultsElement.innerHTML = '';
-
-    fetch('./travel_recommendation_api.json')
-    .then(response => response.json())
-    .then(data => {
-
-        const searchWord= searchVariations(input);
-        // const searchWord= getVariationWord(input);
-
-        console.log('test: '+searchWord);
-
-        //Data object was turn into an array of keys using Object.keys() method
-        //Since data is not an array, the find method can't be used on an object
-        //The find method returns the key which matched the users input
-        const keyArray = Object.keys(data);
-        const key = keyArray.find(key => key.toLowerCase() === searchWord);
-
-        if(key){
-            //Using the key we can reference the property the user input
-            //object[''] access was used because key is a string
-            //if object.property access was used it would return undefined
-            //Example: data['property'] vs data.'property' (wouldnt work because of the '')
-            const destination = data[key];
-            
-            if(key === 'countries'){
-
-                //Iterates array of countries
-                //creates elements with information from json 
-                for(const country of destination){  
-                    //itterates array of cities
-                    for(const city of country.cities){
-                        let placeContainer = document.createElement('div');
-                        let nameElement = document.createElement('h2');
-                        let descriptionElement = document.createElement('p');
-                        let imgElement = document.createElement('img');
-                        let buttonElement = document.createElement('button');
-
-                        imgElement.src = `./pictures/${city.imageUrl}`;
-                        descriptionElement.innerHTML = city.description;
-
-                        nameElement.innerHTML = city.name;
-                        buttonElement.innerHTML = 'Visit';
-
-                        buttonElement.setAttribute('id', 'visit');
-                        imgElement.classList.add('thumbnail');
+//Displays info 
+//Creates elements and adds them to HTML file
+function displayInfo(place){
+    let placeContainer = document.createElement('div');
+    let nameElement = document.createElement('h2');
+    let descriptionElement = document.createElement('p');
+    let imgElement = document.createElement('img');
+    let buttonElement = document.createElement('button');
     
-                        placeContainer.appendChild(imgElement);
-                        placeContainer.appendChild(nameElement);
-                        placeContainer.appendChild(descriptionElement);
-                        placeContainer.appendChild(buttonElement);
-                        resultsElement.appendChild(placeContainer);
-                    }
-
-                }
-            }else{
-                //Iterates thru array of places
-                //creates elements with information from json 
-                for(place of destination){
-                    let placeContainer = document.createElement('div');
-                    let nameElement = document.createElement('h2');
-                    let descriptionElement = document.createElement('p');
-                    let imgElement = document.createElement('img');
-                    let buttonElement = document.createElement('button');
-    
-                    nameElement.innerHTML = place.name;
-                    descriptionElement.innerHTML = place.description;
-                    imgElement.src = `./pictures/${place.imageUrl}`;
-                    buttonElement.innerHTML = 'Visit';
+    nameElement.innerHTML = place.name;
+    descriptionElement.innerHTML = place.description;
+    imgElement.src = `./pictures/${place.imageUrl}`;
+    buttonElement.innerHTML = 'Visit';
                     
-                    buttonElement.setAttribute('id', 'visit');
-                    imgElement.classList.add('thumbnail');
-                    
+    buttonElement.setAttribute('id', 'visit');
+    imgElement.classList.add('thumbnail');          
 
-                    placeContainer.appendChild(imgElement);
-                    placeContainer.appendChild(nameElement);    
-                    placeContainer.appendChild(descriptionElement);
-                    placeContainer.appendChild(buttonElement);
-                    resultsElement.appendChild(placeContainer);
-                }
-            }
-        }else{
-            console.log('no matches found');
-            let placeContainer = document.createElement('div');
-            let messageEle = document.createElement('h4');
-
-            messageEle.innerHTML = 'Destination not found. <br> Try: country, beach, temple.' 
-
-            placeContainer.setAttribute('id','message')
-
-            placeContainer.appendChild(messageEle);    
-            resultsElement.appendChild(placeContainer);
-        }
-    })
-    .catch(error=>{
-        console.error('Error: ', error);
-        console.log('not able to fetch data');
-    })
+    placeContainer.appendChild(imgElement);
+    placeContainer.appendChild(nameElement);    
+    placeContainer.appendChild(descriptionElement);
+    placeContainer.appendChild(buttonElement);
+    resultsElement.appendChild(placeContainer);
 }
 
+//Searches for destination information searched by the user
+function searchDestination(){
+    var input = document.getElementById('destination').value.toLowerCase();
+    resultsElement.innerHTML = '';
+
+    if(input){
+        fetch('./travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+
+            const searchWord= searchVariations(input);
+            // const searchWord= getVariationWord(input);
+
+            console.log('word: '+searchWord);
+
+            //Data object was turn into an array of keys using Object.keys() method
+            //Since data is not an array, the find method can't be used on an object
+            //The find method returns the key which matched the users input
+            const keyArray = Object.keys(data);
+            const key = keyArray.find(key => key.toLowerCase() === searchWord);
+
+            if(key){
+                //Using the key we can reference the property the user input
+                //object[''] access was used because key is a string
+                //if object.property access was used it would return undefined
+                //Example: data['property'] vs data.'property' (wouldnt work because of the '')
+                const destination = data[key];
+                
+                if(key === 'countries'){
+                    //Iterates array of countries
+                    //creates elements with information from json 
+                    for(const country of destination){  
+                        //itterates array of cities
+                        for(const city of country.cities){
+                            displayInfo(city);
+                        }
+                    }
+                }else{
+                    //displays temples or beaches
+                    //Iterates thru array of places
+                    for(place of destination){
+                        displayInfo(place);
+                    }
+                }
+            }else{
+                var citiesArray=[];
+                data.countries.forEach(country => {
+                    const cities= country.cities.filter(city => city.name.toLowerCase().includes(searchWord));
+                    citiesArray = citiesArray.concat(cities); // adds array values from cities
+                });
+
+                //if there is no results from the arrray display message
+                if(citiesArray.length ===0){
+                    console.log('no matches found');
+                    let placeContainer = document.createElement('div');
+                    let messageEle = document.createElement('h4');
+
+                    messageEle.innerHTML = 'Destination not found. <br> Try: country, beach, temple.' 
+
+                    placeContainer.setAttribute('id','message')
+
+                    placeContainer.appendChild(messageEle);    
+                    resultsElement.appendChild(placeContainer);
+                }else{
+                    //else display results from the array
+                    for(city of citiesArray){
+                        displayInfo(city);
+                     }
+                }
+            }
+        })
+        .catch(error=>{
+            console.error('Error: ', error);
+            console.log('not able to fetch data');
+        })
+    }else{
+        let userInput = document.getElementById('destination');
+        alert('Please enter a destination')
+        userInput.focus();
+    }
+}
+
+//resets input values and search results
 function reset(){
     let resultsEle = document.getElementById('results');
     let destinationEle = document.getElementById('destination');
